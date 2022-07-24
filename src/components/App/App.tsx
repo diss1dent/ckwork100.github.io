9c9df020b8ec498ce666be84ae32931a3d1c6d81
AntonL <ckwork100@gmail.com>
2022-07-24 15:03:39 +0300

    updates rollback

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="app">
+            <div className="card">
+                1111
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
